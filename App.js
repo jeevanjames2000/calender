8761ac8fbb722409a651f553dc1eeb8c70ffb6d2
jeevanjames2000 <jeevanjames2000@gmail.com>
2024-12-17 16:28:26 +0530

    c

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useEffect, useState } from "react";
+import { Button, Text, View, StyleSheet, Image } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
 export default function App() {
+  const [userInfo, setUserInfo] = useState(null);
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId:
+        "281834706725-pjmcmkmdo7hjk9m3qpec26iitnn922mp.apps.googleusercontent.com",
+      androidClientId:
+        "281834706725-pjmcmkmdo7hjk9m3qpec26iitnn922mp.apps.googleusercontent.com",
+      offlineAccess: true,
+      scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+      offlineAccess: true,
+      hostedDomain: "",
+      forceCodeForRefreshToken: false,
+      accountName: "",
+      iosClientId: "<FROM DEVELOPER CONSOLE>",
+      googleServicePlistPath: "",
+      openIdRealm: "",
+      profileImageSize: 120,
+    });
+  }, []);
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+      if (isSuccessResponse(response)) {
+        setState({ userInfo: response.data });
+      } else {
+      }
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.IN_PROGRESS:
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            break;
+          default:
+        }
+      } else {
+      }
+    }
+  };
+  const signOut = async () => {
+    try {
+      await GoogleSignin.signOut();
+      setUserInfo(null);
+    } catch (error) {
+      console.log("Error signing out", error);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {!userInfo ? (
+        <Button title="Sign in with Google" onPress={signIn} />
+      ) : (
+        <View>
+          <Text>Welcome, {userInfo.user.name}!</Text>
+          <Text>Email: {userInfo.user.email}</Text>
+          <Image
+            source={{ uri: userInfo.user.photo }}
+            style={styles.profileImage}
+          />
+          <Button title="Sign out" onPress={signOut} />
+        </View>
+      )}
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginTop: 10,
   },
 });
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+// SHA1 Fingerprint    98:00:DC:D8:7A:0C:AE:E3:FE:75:A5:C7:52:82:AD:12:08:B6:4A:1C
